690922dd5a6e20d3fe720d6e2c6b4d041173d338
Devin MacGillivray <devhmac@gmail.com>
2025-09-29 08:19:25 -0600

    set up treesitter lsp for ts

--- init.el
+++ init.el
@@ -107,7 +107,7 @@
        ;;pdf               ; pdf enhancements
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
-       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
+       tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
@@ -168,7 +168,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       web               ; the tubes
+       (web +lsp +tree-sitter)               ; the tubes
        ;;yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Emacs Init steps for typescript treesitter support
+
+Adds TS/TSX recipie
+``` shell
+(progn
+  (require 'treesit)
+  (setq treesit-language-source-alist
+        (append
+         '((typescript "https://github.com/tree-sitter/tree-sitter-typescript" "v0.23.2" "typescript/src")
+           (tsx        "https://github.com/tree-sitter/tree-sitter-typescript" "v0.23.2" "tsx/src")
+           (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.2" "src"))
+         (or treesit-language-source-alist '())))
+  (message "Added TypeScript/TSX/JS recipes to treesit-language-source-alist"))
+
+```
